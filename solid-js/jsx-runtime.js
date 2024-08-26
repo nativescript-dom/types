cf6e542da35ab2361fc3934de2efb8dd63a31623
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-08-26 13:50:52 +0500

    solidjs: add jsx-runtime.js

--- solid-js/jsx-runtime.js
+++ solid-js/jsx-runtime.js
@@ -1,11 +1,11 @@
-import h from 'solid-js/h';
+import { createComponent } from "@nativescript-community/solid-js/src/renderer";
 
 function Fragment(props) {
   return props.children;
 }
 
 function jsx(type, props) {
-  return h(type, props);
+  return createComponent(type, props);
 }
 
 export { Fragment, jsx, jsx as jsxDEV, jsx as jsxs };
